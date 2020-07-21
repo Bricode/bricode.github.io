d67a203d437262334becdc6a15f0ed3ae3ef1a49
Blake Chalmers (1000064560) <CHALBR1@student.op.ac.nz>
2020-07-21 17:01:47 +1200

    Purging files from last year

--- assets/Js/contact.js
+++
@@ -1,9 +0,0 @@
-let submitComment = document.getElementById("submit");
-
-function submitBton() {
-  console.log(document.getElementById("Fname").value);
-  document.getElementById("Centering").innerHTML =
-    "Thank you for your message " + document.getElementById("Fname").value;
-}
-
-submitComment.addEventListener("click", submitBton);
